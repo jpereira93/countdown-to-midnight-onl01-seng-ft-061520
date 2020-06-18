a29707c725ca8c96f8225fa3ec65c770aad8e825
Jessica Pereira <jcpereira070@gmail.com>
2020-06-18 18:25:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 #write your code here
-def countdown(seconds_to_midnight)
-  while seconds_to_midnight > 0
-    puts “#{seconds_to_midnight} SECOND(S)!”
-    seconds_to_midnight -= 1
+def countdown(x)
+  while x > 0
+    puts "#{x} SECOND(S)!" 
+    x -= 1
   end
   “HAPPY NEW YEAR!”
 end
