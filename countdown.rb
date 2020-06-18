fb18b9bb73f38e7d49dc388632492a98d50fa0db
Jessica Pereira <jcpereira070@gmail.com>
2020-06-18 18:27:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,9 +6,11 @@ def countdown(x)
 end
   “HAPPY NEW YEAR!”
 end
+
+
 def countdown_with_sleep(x)
   while x > 0
-    puts "#{x} SECOND(S)!”
+    puts "#{x} SECOND(S)!"
     sleep(1)
     x -= 1
   end
